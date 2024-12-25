f861bfc900891292038bf02387773adaefb079dc
Ars472 <arsendautov6@gmail.com>
2024-12-25 22:11:52 +0600

    Добавлено решение задачи Power и тесты

---
+++ src/test/java/powerTest/PowerCalculatorTest.java
@@ -0,0 +1,14 @@
+package powerTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import power.PowerCalculator;
+
+public class PowerCalculatorTest {
+    @Test
+    public void testPower(){
+        Assert.assertEquals(PowerCalculator.power(230,10),2300);
+        Assert.assertEquals(PowerCalculator.power(110,3),330);
+        Assert.assertEquals(PowerCalculator.power(480,20),9600);
+    }
+}
